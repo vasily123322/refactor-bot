1ca32833dcf6d01452948f832f5041cef2eeb611
vasily123322 <vasilyiusi@gmail.com>
2026-08-08 07:25:56 +0300

    feat: add content domain package

---
+++ app/domain/content/__init__.py
@@ -0,0 +1,11 @@
+from .document import (
+    POST_DOCUMENT_SCHEMA_VERSION,
+    PostDocument,
+    PostDocumentError,
+)
+
+__all__ = [
+    "POST_DOCUMENT_SCHEMA_VERSION",
+    "PostDocument",
+    "PostDocumentError",
+]
