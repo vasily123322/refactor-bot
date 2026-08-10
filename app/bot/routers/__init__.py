3d89b5185267dd1f13d5595a1fb06500ca1f22e9
vasily123322 <vasilyiusi@gmail.com>
2026-08-10 03:21:15 +0300

    feat: register canonical publication content-plan router

--- app/bot/routers/__init__.py
+++ app/bot/routers/__init__.py
@@ -7,6 +7,7 @@ from .navigation import router as navigation_commands
 from .chats import router as chats_commands
 from .post_editor import router as post_editor_commands
 from .posting_publish import router as posting_publish_commands
+from .content_plan_publications import router as content_plan_publication_commands
 from .content_plan import router as content_plan_commands
 from .settings import router as settings_commands
 from .admin import router as admin_commands
@@ -25,6 +26,7 @@ main_router.include_router(navigation_commands)
 main_router.include_router(chats_commands)
 main_router.include_router(post_editor_commands)
 main_router.include_router(posting_publish_commands)
+main_router.include_router(content_plan_publication_commands)
 main_router.include_router(content_plan_commands)
 main_router.include_router(settings_commands)
 main_router.include_router(admin_commands)
