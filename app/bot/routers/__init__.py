f6c8f3606549f4fe7555a45f3dc50e34ea1124d3
vasily123322 <vasilyiusi@gmail.com>
2026-08-13 14:26:16 +0300

    ui: install pending cancellation migration control

--- app/bot/routers/__init__.py
+++ app/bot/routers/__init__.py
@@ -8,6 +8,7 @@ from .chats import router as chats_commands
 from .post_editor import router as post_editor_commands
 from .posting_publish import router as posting_publish_commands
 from .content_plan_publication import router as content_plan_publication_commands
+from .content_plan_migration_controls import router as content_plan_migration_controls
 from .content_plan import router as content_plan_commands
 from .settings import router as settings_commands
 from .admin import router as admin_commands
@@ -31,6 +32,7 @@ main_router.include_router(chats_commands)
 main_router.include_router(post_editor_commands)
 main_router.include_router(posting_publish_commands)
 main_router.include_router(content_plan_publication_commands)
+main_router.include_router(content_plan_migration_controls)
 main_router.include_router(content_plan_commands)
 main_router.include_router(settings_commands)
 main_router.include_router(admin_commands)
