33eeb9adca809134947c4636a20a7f100b372476
vasily123322 <vasilyiusi@gmail.com>
2026-09-19 05:37:16 +0300

    test(db): advance expected alembic head

--- tests/test_posttask_schema_drop.py
+++ tests/test_posttask_schema_drop.py
@@ -20,7 +20,7 @@ from app.services.publication_autodelete_lease import PublicationAutodeleteLease
 
 
 PRE_DROP = "20260918_0014"
-HEAD = "20260919_0017"
+HEAD = "20260919_0018"
 
 
 def _run_alembic(
